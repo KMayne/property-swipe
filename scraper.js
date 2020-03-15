a0b8376d6311c599cb69e282cda445d2006d993b
Kian Mayne <git@kianmayne.co.uk>
2020-03-15 23:28:31 +0000

    Process listings into expected format & use logging lib

--- scraper.js
+++ scraper.js
@@ -5,11 +5,18 @@ const fs = require('fs').promises;
 const { JSDOM } = require('jsdom');
 const Bottleneck = require('bottleneck');
 const maps = require("@googlemaps/google-maps-services-js");
+const winston = require('winston');
 
 const secrets = require('./secrets.json');
 
 const mapsClient = new maps.Client();
-
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console({
+    stderrLevels: ['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly']
+  })]
+});
 const limiter = new Bottleneck({
   minTime: 2000
 });
@@ -28,14 +35,13 @@ async function cached(func, cacheFileName, maxAgeHours) {
     return funcResult;
   } else {
     const fileText = await fs.readFile(cacheFileName, { encoding: 'utf8' });
-    console.log('Parsing file', cacheFileName)
     return JSON.parse(fileText);
   }
 }
 
 async function getMapPage() {
   return cached(async () => {
-    console.info('Fetching map page');
+    logger.info('Fetching map page');
     const response = await rateLimitedFetch(secrets.zooplaSearchURL);
     return response.text();
   }, './data/map.html', 0.5);
@@ -43,7 +49,7 @@ async function getMapPage() {
 
 async function getListings() {
   return cached(async () => {
-    console.info('Parsing map page for listings');
+    logger.info('Parsing map page for listings');
     const dom = new JSDOM(await getMapPage());
     const scriptElems = Array.from(dom.window.document.querySelectorAll('script'));
     const dataScript =
@@ -58,11 +64,15 @@ async function getListings() {
   }, './data/listings.json', 0.5);
 }
 
+function getListingURL(listingID) {
+  return `https://www.zoopla.co.uk/to-rent/details/${listingID}`
+}
+
 async function fetchListingDetailsPage(listingID) {
   await fs.mkdir('./data/details-html/', { recursive: true });
   return cached(async () => {
-    console.info('Fetching property details page for', listingID);
-    const listingUrl = `https://www.zoopla.co.uk/to-rent/details/${listingID}`;
+    logger.info('Fetching property details page for ' + listingID);
+    const listingUrl = getListingURL(listingID);
     const response = await rateLimitedFetch(listingUrl);
     if (!response.ok) {
       throw new Error(`Error retrieving response for ${listingID}. Status code ${response.status}, response: ${await response.text()}`);
@@ -71,11 +81,11 @@ async function fetchListingDetailsPage(listingID) {
   }, `./data/details-html/${listingID}.html`, 24 * 7);
 }
 
-async function getListingDetailsJson(listingID) {
+async function getListingDetails(listingID) {
   const jsonLDSelector = 'script[type="application/ld+json"]';
   await fs.mkdir('./data/details-json/', { recursive: true });
   return cached(async () => {
-    console.info('Parsing property details page for', listingID);
+    logger.info('Parsing property details page for ' + listingID);
     const listingPageText = await fetchListingDetailsPage(listingID);
     const dom = new JSDOM(listingPageText);
     const jsonLDElems = dom.window.document.querySelectorAll(jsonLDSelector);
@@ -96,28 +106,42 @@ async function getCommuteTimes(latitude, longitude) {
     mode: 'transit',
     units: 'metric'
   }});
+  const times = response.data.rows[0];
+  if (times.status !== 'OK') return { workCommuteMins: 0, gfCommuteMins: 0 };
   const [workCommuteMins, gfCommuteMins] =
-    response.data.rows[0].elements
-    .map(elem => Math.round(elem.duration.value / 60));
+    workCommuteResults.map(elem => Math.round(elem.duration.value / 60));
   return { workCommuteMins, gfCommuteMins };
 }
 
 async function processZooplaListing(listing) {
   await fs.mkdir('./data/processed/', { recursive: true });
-
-}
-
-async function getProcessedListings() {
-  console.info('Retrieving processed listings');
-  const listings = await getListings();
-  return Promise.all(
-    listings.map(listing => processZooplaListing(listing)));
+  const listingID = listing.listing_id;
+  cached(async () => {
+    const [commuteTimes, details] = await Promise.all([
+      getCommuteTimes(listing.lat, listing.lon),
+      getListingDetails(listingID)
+      ]);
+    const priceRegex = /£(?<price>[\d,]+) pcm/;
+    const price = Number(details.description
+      .match(priceRegex).groups.price
+      .replace(/,/g, ''));
+    return {
+      summary: details.name.replace(' to rent', ''),
+      price,
+      ...commuteTimes,
+      locality: details.address.addressLocality,
+      listingID,
+      link: getListingURL(listingID),
+      photos: details.photo.map(imgObject => imgObject.contentUrl)
+    }
+  }, `./data/processed/${listingID}.json`, 24 * 7);
 }
 
 async function main() {
   // Ensure the data directory exists
   await fs.mkdir('./data', { recursive: true });
-  return await getCommuteTimes('51.526253', '-0.067585');
+  const listings = await getListings();
+  await Promise.all(listings.map(listing => processZooplaListing(listing)));
 }
 
 main()
