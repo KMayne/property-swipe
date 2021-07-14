5aa3b7d25b574e0a36173efd997583f56a3eed97
Kian Mayne <git@kianmayne.co.uk>
2021-07-14 22:57:25 +0100

    Fix file logging

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,21 @@
+const path = require('path');
 const winston = require('winston');
 
-const transports = [new winston.transports.Console()];
+const mainFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.errors({ stack: true }),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+const transports = [new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize({ all: true }),
+    mainFormat
+  )
+})];
 if (process.env.NODE_ENV !== 'development') {
   transports.push(new winston.transports.File({
-    filename: '../logs/ps.log',
+    filename: path.resolve(__dirname, '../logs/ps.log'),
     maxFiles: 10,
     maxsize: 10 * Math.pow(2, 20), // 10MiB
     tailable: true
@@ -12,11 +24,6 @@ if (process.env.NODE_ENV !== 'development') {
 
 module.exports = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(
-    winston.format.colorize({ all: true }),
-    winston.format.timestamp(),
-    winston.format.errors({ stack: true }),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
+  format: mainFormat,
   transports
 });
